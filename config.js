21b757534b8b9f4729f33ebbdd27434e50e31c94
joesamax <johan.galipo@gmail.com>
2020-06-15 03:59:06 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'test',
+		wp_ws_url : 'https://alizesdeveloppement.fr/wp-appkit-api/test',
+		wp_url : 'https://alizesdeveloppement.fr',
+		theme : 'q-android',
+		version : '1.0.0.6',
+		app_type : 'phonegap-build',
+		app_title : 'Vcard',
+		app_platform : 'android',
+		app_path: '',
+		gmt_offset : 2,
+		debug_mode : 'off',
+		auth_key : 'sU[SrV=IproZrxfDZ=aBErC+/LLu) i` a=%HAIe(t!G}+#5=mdYKp%@WLxNNvFw',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
